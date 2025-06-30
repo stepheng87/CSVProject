b38ac6a39cdba8c3f47ee07caf3a169b76219489
stepheng87 <scooba75@gmail.com>
2025-06-30 16:36:17 -0500

    sitka highs graph done, creating secondary plot

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 
-filename = r'C:\Users\Stephen_Gonzales1\Documents\AdvPython\CSV\sitka_weather_07-2018_simple.csv'
+filename = r'C:\Users\Stephen_Gonzales1\Documents\AdvPython\CSV\sitka_weather_2018_simple.csv'
 
 with open(filename) as f:
     reader= csv.reader(f)
@@ -30,7 +30,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
 # Format plot
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.title("Daily high temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
